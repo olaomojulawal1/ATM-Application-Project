b4c1e23c2c26b05b520710f4661aefa687130714
olaomoju lawal <olaomojulawal@gmail.com>
2024-10-17 15:17:41 -0400

    first commit

---
+++ src/main/java/bptn/ATMAPPLICATION/App.java
@@ -0,0 +1,13 @@
+package bptn.ATMAPPLICATION;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
